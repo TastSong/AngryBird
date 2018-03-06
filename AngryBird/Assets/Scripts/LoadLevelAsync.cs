ca2cd7a4310c2f13fdf94c6470d779a8465ea007
Tast <TengfeiSong1@Gmail.com>
2018-03-06 10:07:58 +0800

    scr

--- AngryBird/Assets/Scripts/LoadLevelAsync.cs
+++ AngryBird/Assets/Scripts/LoadLevelAsync.cs
@@ -8,7 +8,7 @@ public class LoadLevelAsync : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        Screen.SetResolution(800, 500, false);
+        Screen.SetResolution(1600, 1000, false);
         Invoke("Loading", 2);
 	}
 	void Loading()
